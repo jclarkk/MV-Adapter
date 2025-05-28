1d38c597193c8eac25dc67ce8823105a5b1d540a
Jonathan Clark <jonathan@hexa3d.io>
2025-05-28 20:00:50 +0300

    Fix pb_solver compile

--- pb_solver/pb_solver.cpp
+++ pb_solver/pb_solver.cpp
@@ -1,35 +1,23 @@
 #include <torch/extension.h>
-
 #include <vector>
 
-// CUDA forward declarations
-
-void pb_solver_run_cuda(
-    torch::Tensor A,
-    torch::Tensor X,
-    torch::Tensor B,
-    torch::Tensor Xbuf,
-    int num_iters
-);
-
-// C++ interface
+// Forward declaration
+void pb_solver_run_cuda(torch::Tensor A, torch::Tensor X, torch::Tensor B, torch::Tensor Xbuf, int num_iters);
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.device().is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
 #define CHECK_INPUT(x) CHECK_CUDA(x); CHECK_CONTIGUOUS(x)
 
-void pb_solver_run(
-    torch::Tensor A,
-    torch::Tensor X,
-    torch::Tensor B,
-    torch::Tensor Xbuf,
-    int num_iters
-) {
+void pb_solver_run(torch::Tensor A, torch::Tensor X, torch::Tensor B, torch::Tensor Xbuf, int num_iters) {
     CHECK_INPUT(A);
     CHECK_INPUT(X);
     CHECK_INPUT(B);
     CHECK_INPUT(Xbuf);
 
     pb_solver_run_cuda(A, X, B, Xbuf, num_iters);
-    return;
+}
+
+// Expose to Python
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+    m.def("pb_solver_run", &pb_solver_run, "Poisson Blending Solver (CUDA)");
 }
